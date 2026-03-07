456d29cb6e4f466f8ce5556d4935746c6002b6f6
Durgadevi <durgadevi112005@gmail.com>
2026-03-07 12:11:24 +0530

    program added

---
+++ Anagram.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Anagram {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.next();
+        String s2 = sc.next();
+
+        if(s1.length() != s2.length()){
+            System.out.println("It is not a Anagram");
+            return;
+        }
+
+        char[] arr1 = s1.toCharArray();
+        char[] arr2 = s2.toCharArray();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        if(Arrays.equals(arr1,arr2)){
+            System.out.println("Yes,It is Anagram");
+        }else{
+            System.out.println("It is not a Anagram");
+        }
+
+    }
+    
+}
