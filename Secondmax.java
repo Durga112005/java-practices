d9cbd478518169eda970b29ca32b8c1bd0d00a20
Durgadevi <durgadevi112005@gmail.com>
2026-03-10 21:14:30 +0530

    program added

--- Secondmax.java
+++ Secondmax.java
@@ -1,39 +1,34 @@
-import java.util.Scanner;
-
+import java.util.*;
 public class Secondmax {
-   public Secondmax() {
-   }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n<2){
+            System.out.println("Secondmax not possible");
+        }
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
 
-   public static void main(String[] var0) {
-      Scanner var1 = new Scanner(System.in);
-      int var2 = var1.nextInt();
-      if (var2 < 2) {
-         System.out.println("can't possible to find second maximum element");
-      } else {
-         int[] var3 = new int[var2];
+        int max = arr[0];
+        int secondmax = Integer.MIN_VALUE;
 
-         for(int var4 = 0; var4 < var2; ++var4) {
-            var3[var4] = var1.nextInt();
-         }
-
-         int var7 = var3[0];
-         int var5 = Integer.MIN_VALUE;
-
-         for(int var6 = 1; var6 < var2; ++var6) {
-            if (var3[var6] > var7) {
-               var5 = var7;
-               var7 = var3[var6];
-            } else if (var3[var6] < var7 && var3[var6] > var5) {
-               var3[var6] = var5;
+        for(int i=1;i<n;i++){
+            if(arr[i]>max){
+                secondmax = max; 
+                max = arr[i];
+            }else if(arr[i]<max && arr[i]>secondmax){
+                secondmax = arr[i];
             }
-         }
+        }
 
-         if (var5 == Integer.MIN_VALUE) {
-            System.out.println("Secondmax not found");
-         } else {
-            System.out.println("Second maximum element is :" + var5);
-         }
-
-      }
-   }
+        if(secondmax == Integer.MIN_VALUE){
+            System.out.println("secondmax not found");
+        }else{
+            System.out.println("secondmax found at" +secondmax);
+        }
+        
+    }
+    
 }
