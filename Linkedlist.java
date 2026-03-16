202bf5325586f4ddfec98b81390ecde918351ab4
Durgadevi <durgadevi112005@gmail.com>
2026-03-16 15:38:34 +0530

    program added

---
+++ Linkedlist.java
@@ -0,0 +1,16 @@
+import java.util.LinkedList;
+
+public class Linkedlist{
+    public static void main(String[] args) {
+
+        LinkedList<String> list  =  new LinkedList<>();
+
+        list.add("Durga");
+        list.add("Goki");
+        list.add("Ram");
+
+        System.out.print("Students :");
+        System.out.println(list);
+
+    }
+}
